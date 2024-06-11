04fcc8318baa8e22bcc2595c9a6073585055c72d
fitran hasby <hasbyassyadiqi@gmail.com>
2024-06-11 08:22:43 +0700

    Project section v2 (dekstop)

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -2,27 +2,25 @@ import { projects } from "../constants/Index"
 
 export const ProjectSection = () => {
   return (
-    <div className="h-screen flex  justify-center flex-col  lg:-mt-12">
-<div className="border-b  border-neutral-900 pb-4">
-        <h1 className="my-24 text-center text-4xl font-bold lg:-mt-12 md:mt-10 sm:mt-10 tracking-[.10em]">Projects</h1>
+    <div className="h-screen" >
+        <h1 id="projects" className="my-24 text-center text-4xl font-bold  tracking-[.10em]">Projects</h1>
         <div>
+            <div className="grid grid-rows-2 grid-cols-3 gap-5 px-5">
             {projects.map((item, index) => (
-            <div key={index} className="flex flex-wrap ml-10 my-20  lg:justify-center">
-                    <div className="w-full lg:w-1/4">
-                    <img src={item.Image} width={150} height={150} alt={item.title} className="mb-66 rounded" />
+                <div key={index} className="rounded-lg py-2 px-2 shadow-xl bg-white/5 ring-1 ring-white/20 backdrop-blur-4xl">
+                    <img src={item.Image} className="mb-2 w-full h-auto rounded-lg"  alt={item.title}  />
+                    <div className="flex space-x-56">
+                    <h6 className="mb-2  font-semibold">{item.title}</h6>
+                    <h6 className="text-neutral-400">{item.date}</h6>
                     </div>
-                <div className="w-full max-w-xl lg:w-3/4">
-                    <h6 className="mb-2 font-semibold">{item.title}</h6>
-                    <p className="mb-4 text-neutral-400">{item.description}</p>
+                    <p className=" text-neutral-400 mb-14">{item.description}</p>
                     {item.technologies.map((tech, index) => (
-                        <span key={index} className="mr-2 rounded border border-teal-400 bg-teal-400 bg-opacity-20 px-1">{tech}</span>
-                    ))}
-                </div>
-                
-            </div>
+                        <span key={index} className="mr-2 rounded border-[1px] border-teal-400 bg-teal-400 bg-opacity-5 px-1">{tech}</span>
+                    ))}   
+               </div>          
             ))}
+            </div>
         </div>
-    </div>
 </div>
     
     
--- src/constants/Index.jsx
+++ src/constants/Index.jsx
@@ -3,12 +3,31 @@ import project1img from "../assets/project1img.png"
 export const navItems = [
     {label: 'Home', href: '#home'},
     {label: 'About', href: '#about'},
-    {label: 'Projects', href: '#'},
+    {label: 'Projects', href: '#projects'},
     {label: 'Contact', href: '#'}
 ]
 
 export const projects = [
     {title: "Landing Page",
+     date: "12/23",
+     Image: project1img,
+     description: "lorem ipsum ler dolor amet",
+     technologies: ["HTML", "CSS", "Javascript"]
+    },
+    {title: "Landing Page",
+     date: "12/23",
+     Image: project1img,
+     description: "lorem ipsum ler dolor amet",
+     technologies: ["HTML", "CSS", "Javascript"]
+    },
+    {title: "Landing Page",
+     date: "12/23",
+     Image: project1img,
+     description: "lorem ipsum ler dolor amet",
+     technologies: ["HTML", "CSS", "Javascript"]
+    },
+    {title: "Landing Page",
+     date: "12/23", 
      Image: project1img,
      description: "lorem ipsum ler dolor amet",
      technologies: ["HTML", "CSS", "Javascript"]
